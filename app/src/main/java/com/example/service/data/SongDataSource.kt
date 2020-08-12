248db8911b4cf718b398c2bad354c1116502b1ee
dinhthuyha <54362507+dinhthuyha@users.noreply.github.com>
2020-08-12 15:36:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/service/data/SongDataSource.kt
@@ -0,0 +1,4 @@
+package com.example.service.data
+
+interface SongDataSource {
+}
---
+++ app/src/main/java/com/example/service/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.service.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.service.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
